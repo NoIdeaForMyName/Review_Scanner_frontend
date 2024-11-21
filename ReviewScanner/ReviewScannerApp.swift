a83be49f22bd429c71d300b98d6d53693d4ce166
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-11-21 11:41:22 +0100

    Initial Commit

---
+++ ReviewScanner/ReviewScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewScannerApp.swift
+//  ReviewScanner
+//
+//  Created by Michał Maksanty on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReviewScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReviewScannerTests/ReviewScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewScannerTests.swift
+//  ReviewScannerTests
+//
+//  Created by Michał Maksanty on 21/11/2024.
+//
+
+import Testing
+@testable import ReviewScanner
+
+struct ReviewScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
