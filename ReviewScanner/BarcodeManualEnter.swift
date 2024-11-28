3240a69dc35e06a3eadbe6b97a81d6082c83cda5
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-11-28 19:51:42 +0100

    Fixed manual entering (TODO: focus on field click; focus on first free field ALWAYS)

--- ReviewScanner/BarcodeManualEnter.swift
+++ ReviewScanner/BarcodeManualEnter.swift
@@ -11,55 +11,85 @@ struct BarcodeManualEnter: View {
     let infoText: String
     
     @State private var barcode: [String] = Array(repeating: "", count: 13) // 13 pól na cyfry
+    @State private var barcodeEntered: Bool = false
     
     // spacing after 1 and 7 idx
     var spacingIdx: [Int] = [1, 7]
     
-    var body: some View {
+    @FocusState private var focusedIndex: Int?
         
-        VStack {
-            Text(infoText)
-                //.font(.caption2)
+    
+    var body: some View {
+        NavigationStack {
             
-            HStack(spacing: 5) {
-                ForEach(0..<13, id: \.self) { index in
-                    if spacingIdx.contains(index) {
-                        Spacer()
-                    }
-                    VStack {
-                        // Pole tekstowe na cyfrę
-                        TextField("", text: $barcode[index])
-                        //.frame(width: 30, height: 40)
-                            //.multilineTextAlignment(.center)
-                            .keyboardType(.numberPad)
-                            .onChange(of: barcode[index]) { newValue in
-                                // Ograniczenie do jednej cyfry
-                                if newValue.count > 1 {
-                                    barcode[index] = String(newValue.prefix(1))
+            VStack {
+                Text(infoText)
+                    .font(.system(size: 22))
+                    .padding(.bottom, 10)
+                
+                HStack(spacing: 5) {
+                    ForEach(0..<13, id: \.self) { index in
+                        if spacingIdx.contains(index) {
+                            Spacer()
+                        }
+                        VStack {
+                            TextField("_", text: $barcode[index])
+                                .focused($focusedIndex, equals: index)
+                                .keyboardType(.numberPad)
+                                .onChange(of: barcode[index]) {
+                                    barcodeValueChanged(currIdx: index)
                                 }
-                                // Usuwanie niedozwolonych znaków
-                                if !newValue.allSatisfy({ $0.isNumber }) {
-                                    barcode[index] = ""
-                                }
-                            }
-                        
-                        // Linia pod polem tekstowym
-                        Text("_")
-                            .font(.title2)
-                            .foregroundColor(.black)
+                            
+                        }
                     }
                 }
+                .padding(.bottom, 10)
             }
-            .padding(.bottom, 20)
+            .padding()
+            .background(Color.button, in: RoundedRectangle(cornerRadius: 50))
+//            .frame(maxHeight: .infinity)
+//            .background(Gradient(colors: gradientColors))
+            
+            .navigationDestination(isPresented: $barcodeEntered) {
+                TestFoundBarcode(barcode: barcodeToString(barcode))
+            }
+            
         }
-        .padding()
-        .background(Color.button, in: RoundedRectangle(cornerRadius: 50))
         
     }
+    
+    func barcodeValueChanged(currIdx: Int) -> Void {
+        var entered = barcode[currIdx]
+        entered = entered.count > 2 ? String(entered.prefix(2)) : entered
+        
+        if entered.count < 2 {
+            if !entered.allSatisfy({ $0.isNumber }) {
+                entered = ""
+            }
+            barcode[currIdx] = entered
+            
+            if entered.isEmpty {
+                focusedIndex = currIdx - 1
+            }
+            
+            if currIdx == barcode.count - 1 {
+                barcodeEntered = true
+            }
+        }
+        else if !entered.isEmpty && currIdx + 1 <= barcode.count - 1 {
+            barcode[currIdx] = String(entered.first!)
+            barcode[currIdx + 1] = String(entered.last!)
+            focusedIndex = currIdx + 1 // Przejdź do następnego pola
+        }
+    }
+    
+    func barcodeToString(_ barcode: [String]) -> String {
+        let joined = barcode.joined(separator: "")
+        return joined
+    }
+    
 }
 
 #Preview {
     BarcodeManualEnter(infoText: "Doesn't work? Enter barcode manually:")
-        .frame(maxHeight: .infinity)
-        .background(Gradient(colors: gradientColors))
 }
