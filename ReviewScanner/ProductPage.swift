b427664a0b324bea3a386d2759a81b3dfa44321a
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2024-12-05 19:56:29 +0100

    Add product page

---
+++ ReviewScanner/ProductPage.swift
@@ -0,0 +1,124 @@
+import SwiftUI
+
+struct ProductPage: View {
+    let productName: String
+    let productImageURL: String
+    let reviewAverage: String
+    let productDescription: String
+    let reviews: [ProductReviewView]
+    
+    @State private var isDescriptionExpanded: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                AsyncImage(url: URL(string: productImageURL)) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 250, height: 250)
+                            .clipped()
+                            .cornerRadius(30)
+                    case .failure:
+                        Image(systemName: "xmark.octagon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 250, height: 250)
+                            .foregroundColor(.red)
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+                
+                HStack() {
+                    Image(systemName: "star")
+                    
+                    Text(reviewAverage)
+                    
+                    Text(productName)
+                        .font(.headline)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text(productDescription)
+                        .lineLimit(isDescriptionExpanded ? nil : 5)
+                        .animation(.easeInOut, value: isDescriptionExpanded)
+                    
+                    Button(action: {
+                        isDescriptionExpanded.toggle()
+                    }) {
+                        Text(isDescriptionExpanded ? "Show Less" : "Show More")
+                            .font(.caption)
+                            .foregroundColor(.blue)
+                    }
+                }
+                
+                Button(action: {
+                    // TODO: add review
+                }) {
+                    Text("Add review")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.button)
+                        .foregroundColor(.black)
+                        .cornerRadius(8)
+                }
+                
+                VStack {
+                    ForEach(0..<reviews.count, id: \.self) { index in
+                        reviews[index]
+                    }
+                }
+                .padding()
+                .background(Color(.systemBackground))
+                .cornerRadius(20)
+                .shadow(radius: 5)
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .background(Gradient(colors: gradientColors))
+    }
+}
+
+#Preview {
+    ProductPage(
+        productName: "MacBook Pro",
+        productImageURL: "https://images.unsplash.com/photo-1719937206168-f4c829152b91?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        reviewAverage: "4.8",
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        reviews: [
+            ProductReviewView(
+                reviewTitle: "Review 1",
+                reviewBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                nickname: "user1",
+                shop: "shop1",
+                price: "4.99",
+                rating: 1,
+                mediaURLs: [
+                    "https://images.unsplash.com/photo-1600262300671-295cb21f4d06?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9va3xlbnwwfHwwfHx8MA%3D%3D",
+                    "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                ]
+            ),
+            ProductReviewView(
+                reviewTitle: "Review 2",
+                reviewBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                nickname: "user2",
+                shop: "shop2",
+                price: "199.99",
+                rating: 5,
+                mediaURLs: [
+                    "https://images.unsplash.com/photo-1600262300671-295cb21f4d06?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9va3xlbnwwfHwwfHx8MA%3D%3D"
+                ]
+            )
+        ]
+    )
+
+}
+
---
+++ ReviewScanner/ProductReviewView.swift
@@ -0,0 +1,93 @@
+import SwiftUI
+
+struct ProductReviewView: View {
+    let reviewTitle: String
+    let reviewBody: String
+    let nickname: String
+    let shop: String
+    let price: String
+    let rating: Int
+    let mediaURLs: [String]
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                HStack() {
+                    Text("Nickname: \(nickname)")
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    Text("Price: \(String(describing: price))$")
+                        .font(.subheadline)
+                }.padding(.bottom, 10)
+                
+                HStack() {
+                    Text("Shop name: \(shop)")
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    ForEach(0..<5) { index in
+                        Image(systemName: index < rating ? "star.fill" : "star")
+                            .foregroundColor(index < rating ? .yellow : .gray)
+                    }
+                }.padding(.bottom, 10)
+                
+                HStack() {
+                    ForEach(mediaURLs, id: \.self) { mediaURL in
+                        AsyncImage(url: URL(string: mediaURL)) { phase in
+                            switch phase {
+                                case .empty:
+                                    ProgressView()
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                case .failure:
+                                    Image(systemName: "xmark.octagon")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50, height: 50)
+                                        .foregroundColor(.red)
+                                @unknown default:
+                                    EmptyView()
+                                }
+                        }
+                    }
+                }
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            .padding(.top, 10)
+            
+            VStack() {
+                Text(reviewTitle)
+                    .font(.title)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    .padding(.bottom, 5)
+                Text(reviewBody)
+                    .padding(.leading, 10)
+                    .padding(.bottom, 10)
+                    .padding(.trailing, 10)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+    }
+}
+
+#Preview {
+    ProductReviewView(reviewTitle: "Review title", reviewBody: "This is the text of the review", nickname: "user1", shop: "Shop1", price: "99.99", rating: 4, mediaURLs: [
+            "https://images.unsplash.com/photo-1600262300671-295cb21f4d06?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9va3xlbnwwfHwwfHx8MA%3D%3D",
+            "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ])
+}
