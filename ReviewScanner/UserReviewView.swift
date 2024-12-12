06df2251ca3cc77fc41795931a8419890d0b0622
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2024-12-12 19:16:32 +0100

    Add page with user's reviews

--- ReviewScanner/UserReviewView.swift
+++ ReviewScanner/UserReviewView.swift
@@ -1,23 +1,23 @@
 import SwiftUI
 
-struct ProductReviewView: View {
-    let upperLeftLabel: String = "Nickname"
+struct UserReviewView: View {
+    let upperLeftLabel: String = "Product"
     
     let reviewTitle: String
     let reviewBody: String
-    let nickname: String
+    let productName: String
     let shop: String
     let price: String
     let rating: Int
     let mediaURLs: [String]
     
     var body: some View {
-        ReviewView(reviewTitle: reviewTitle, reviewBody: reviewBody, upperLeftLabel: upperLeftLabel, upperLeftValue: nickname, shop: shop, price: price, rating: rating, mediaURLs: mediaURLs)
+        ReviewView(reviewTitle: reviewTitle, reviewBody: reviewBody, upperLeftLabel: upperLeftLabel, upperLeftValue: productName, shop: shop, price: price, rating: rating, mediaURLs: mediaURLs)
     }
 }
 
 #Preview {
-    ProductReviewView(reviewTitle: "Review title", reviewBody: "This is the text of the review", nickname: "user1", shop: "Shop1", price: "99.99", rating: 4, mediaURLs: [
+    UserReviewView(reviewTitle: "Review title", reviewBody: "This is the text of the review", productName: "MacBook Pro", shop: "Shop1", price: "99.99", rating: 4, mediaURLs: [
             "https://images.unsplash.com/photo-1600262300671-295cb21f4d06?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9va3xlbnwwfHwwfHx8MA%3D%3D",
             "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ])
