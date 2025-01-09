5bf6316c81ae7336a8ac017059bc5024f223ecfb
Piotr Zatwarnicki <piotr.l.zatwanricki@gmailcom>
2025-01-09 13:25:53 +0100

    One can now tap on user review and is tranported to product page

--- ReviewScanner/UserReviews/UserReviewsViewModel.swift
+++ ReviewScanner/UserReviews/UserReviewsViewModel.swift
@@ -11,7 +11,9 @@ import Combine
 class UserReviewsViewModel: ObservableObject {
     @Published public var errorData: APIError? // Do przechowywania błędów logowania
     @Published public var reviewData: [ReviewData] = []
+    @Published public var productData: ProductData?
     @Published public var success: Bool = false
+    @Published public var successProd: Bool = false
     @Published public var error: Bool = false
     @Published public var isLoading: Bool = false
 
@@ -30,12 +32,35 @@ class UserReviewsViewModel: ObservableObject {
                     print("Product data fetched successfully")
                     self?.success = true
                 case .failure(let error):
+                    self?.success = false
                     self?.error = true
                     self?.errorData = error
                 }
                 self?.isLoading = false
             }, receiveValue: { [weak self] revData in
                 self?.reviewData = revData
+            })
+    }
+    
+    public func fetchProductData(environmentData: EnvironmentData, id: Int) -> Void {
+        errorData = nil // Wyczyść poprzedni błąd
+        isLoading = true
+
+        productDataCancellable = environmentData.guestService.fetchProductId(id: id)
+            .receive(on: DispatchQueue.main) // Zapewnij, że aktualizacje UI będą na głównym wątku
+            .sink(receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .finished:
+                    print("Product data fetched successfully")
+                    self?.successProd = true
+                case .failure(let error):
+                    self?.successProd = false
+                    self?.error = true
+                    self?.errorData = error
+                }
+                self?.isLoading = false
+            }, receiveValue: { [weak self] prodData in
+                self?.productData = prodData
             })
     }
     
